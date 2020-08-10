04d28ab8ced0334a2391eac135258963f9af4ad8
Kris <1095427410@qq.com>
2020-08-10 22:47:48 +0800

    init project

---
+++ src/main/java/com/kris/leetcode/easy/array/Test.java
@@ -0,0 +1,14 @@
+package com.kris.leetcode.easy.array;
+
+/**
+ * @Description TODO
+ * @Author Kris
+ * @Date 2020/8/10 22:38
+ * 个人公众号：余同学的开发之路
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
