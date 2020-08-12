38135f4aa9197ab1acd3b627106934d2c681ae5e
Kris <1095427410@qq.com>
2020-08-13 00:31:04 +0800

    Max Sub Array

--- src/main/java/com/kris/leetcode/easy/array/MaxSubArray.java
+++ src/main/java/com/kris/leetcode/easy/array/MaxSubArray.java
@@ -18,7 +18,7 @@ package com.kris.leetcode.easy.array;
  *
  * 算法一： 贪心算法：若当前指针所指元素,之前元素的和小于0，则丢弃当前元素之前的数列。
  *
- * 算法二： 动态规划：
+ * 算法二： 动态规划：若前一个元素大于0，则将其加到当前元素上。
  *
  */
 public class MaxSubArray {
@@ -30,7 +30,22 @@ public class MaxSubArray {
     }
 
     /**
+     * 若前一个元素大于 0，则将其加到当前位置上，以此类推，每一次都选出最大的数，赋给 max 最后返回即可。
+     */
+    public int maxSubArray2(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] > 0){
+                nums[i] = nums[i] + nums[i-1];
+            }
+            max = Math.max(nums[i], max);
+        }
+        return max;
+    }
+
+    /**
      * 贪心算法实现，若当前指针所指元素之前的元素的和小于0，则丢弃当前元素之前的数列。丢弃也就是代表将 sum 置为 0 。
+     * 然后记录最大值，整个数组循环下来最大的值即为最大的值。
      */
     public int maxSubArray(int[] nums) {
         int sum = 0;
